7a2f6cf28d6ea6403013629b740058c3262e23da
chandrusanjiv <chandrusanjeev5@gmail.com>
2026-04-03 07:52:59 +0530

    dashbpard correction and alignments

--- src/pages/main/Dashboard/index.js
+++ src/pages/main/Dashboard/index.js
@@ -110,53 +110,57 @@ const Dashboard = () => {
     }, [dispatch, isFetchDashboardData, isFetchAllCrExcel, isFetchByCrNumber]);
 
     return (
-        <div>
-            <div>
+        <div className="dashboard-container">
+            <div className="dashboard-header">
                 <h3>Dashboard</h3>
             </div>
-            <div style={{ display: "flex", justifyContent: "space-between", gap: "20px" }}>
-                <div style={{ width: "60%" }}>
-                    <div className="flexible">
+            <div className="dashboard-layout">
+                <div className="dashboard-left">
+                    <div className="dashboard-cards">
                         <div className="dashboard-card">
                             <div className="dashboard-upload-icon">
-                                <img className="image" src={totalUploadedIcon} alt="Total Uploaded Excel" />
+                                <img className="image" src={totalUploadedIcon} alt="" />
                             </div>
                             <h5 className="title-color">Total Uploaded Excel</h5>
                             <p className="dashboard-card-value">{dashboardData?.totalExcelCount}</p>
                         </div>
                         <div className="dashboard-card">
                             <div className="dashboard-totalCrnumber-icon">
-                                <img className="image" src={totalCrNumberIcon} alt="Total CR Number" />
+                                <img className="image" src={totalCrNumberIcon} alt="" />
                             </div>
-                            <h5 className="title-color">Total CR Number</h5>
+                            <h5 className="title-color">Total Contract Number</h5>
                             <p className="dashboard-card-value">{dashboardData?.totalCrNumberCount}</p>
                         </div>
                         <div className="dashboard-card">
                             <div className="dashboard-complete-icon">
-                                <img className="image" src={toitalCompleteIcon} alt="Completed CR Number" />
+                                <img className="image" src={toitalCompleteIcon} alt="" />
                             </div>
-                            <h5 className="title-color">Completed CR Number</h5>
+                            <h5 className="title-color">Completed Contract Number</h5>
                             <p className="dashboard-card-value">{dashboardData?.completedCrNumberCount}</p>
                         </div>
                         <div className="dashboard-card">
                             <div className="dashboard-pending-icon">
-                                <img className="image" src={totalPendingIcon} alt="Pending CR Number" />
+                                <img className="image" src={totalPendingIcon} alt="" />
                             </div>
-                            <h5 className="title-color">Pending CR Number</h5>
+                            <h5 className="title-color">Pending Contract Number</h5>
                             <p className="dashboard-card-value">{dashboardData?.pendingCrNumberCount}</p>
                         </div>
                     </div>
                     <div className="dashboard-chart-container">
-                        <div className="flexible">
+                        <div className="dashboard-chart-header">
                             <div>
                                 <h4 className="dashboard-card-month-title">Monthly Status</h4>
-                                <p className="dashboard-card-month-subtitle">Monthly Uploads of Excel Status</p>
+                                <p className="dashboard-card-month-subtitle">
+                                    Monthly Uploads of Excel Status
+                                </p>
                             </div>
-                            <div>
-                                <DatePicker picker="year" value={dayjs().year(year)} onChange={(date) => handleYearChange(date)} />
-                            </div>
+                            <DatePicker
+                                picker="year"
+                                value={dayjs().year(year)}
+                                onChange={(date) => handleYearChange(date)}
+                            />
                         </div>
-                        <div style={{ padding: "10px" }}>
+                        <div className="dashboard-chart">
                             <BarChart
                                 labels={labels}
                                 datasets={datasets}
@@ -167,49 +171,60 @@ const Dashboard = () => {
                         </div>
                     </div>
                 </div>
-                <div style={{ width: "40%" }}>
+                <div className="dashboard-right">
                     <div className="dashboard-search-container">
-                        <label style={{ fontWeight: "600", fontSize: "14px" }}>Select CR Number</label>
+                        <label>Select Contract Number</label>
                         <UiSelect
                             isStyle={true}
                             value={selectedCrNumber}
-                            style={{ width: "100%" }}
                             options={crNumberOptions}
                             onChange={(value) => handleChangeCrNumber(value)}
                         />
                     </div>
-                    <div className="flexible">
+                    <div className="dashboard-summary-cards">
                         <div className="dashboard-card">
-                            <div className="dashboard-total-firm-card flexible">
-                                <img className="image" src={totalFirmIcon} alt="Total Firm" />
+                            <div className="dashboard-total-firm-card">
+                                <img className="image" src={totalFirmIcon} alt="" />
                             </div>
-                            <h5 className="dashboard-card-month-subtitle">Total Firm</h5>
-                            <p className="dashboard-crNumber-card">{crNumberSummary?.totalFimNumbers || 0}</p>
+                            <h5 className="dashboard-card-month-subtitle">Total Fim</h5>
+                            <p className="dashboard-crNumber-card">
+                                {crNumberSummary?.totalFimNumbers || 0}
+                            </p>
                         </div>
                         <div className="dashboard-card">
                             <div className="dashboard-toal-qty-card">
-                                <img className="image" src={totalQuantityIcon} alt="Total Quantity" />
+                                <img className="image" src={totalQuantityIcon} alt="" />
                             </div>
                             <h5 className="dashboard-card-month-subtitle">Total Quantity</h5>
-                            <p className="dashboard-crNumber-card">{crNumberSummary?.totalQty || 0}</p>
+                            <p className="dashboard-crNumber-card">
+                                {crNumberSummary?.totalQty || 0}
+                            </p>
                         </div>
                         <div className="dashboard-card">
                             <div className="dashboard-card-label">
-                                <img className="image" src={labeledIcon} alt="Labeled" />
+                                <img className="image" src={labeledIcon} alt="" />
                             </div>
                             <h5 className="dashboard-card-month-subtitle">Labeled</h5>
-                            <p className="dashboard-crNumber-card">{crNumberSummary?.totalLabeledQty || 0}</p>
+                            <p className="dashboard-crNumber-card">
+                                {crNumberSummary?.totalLabeledQty || 0}
+                            </p>
                         </div>
                         <div className="dashboard-card">
-                            <div className="dashboard-total-firm-card flexible">
-                                <img className="image" src={scannedIcon} alt="Scanned" />
+                            <div className="dashboard-total-firm-card">
+                                <img className="image" src={scannedIcon} alt="" />
                             </div>
                             <h5 className="dashboard-card-month-subtitle">Scanned</h5>
-                            <p className="dashboard-crNumber-card">{crNumberSummary?.totalScannedQty || 0}</p>
+                            <p className="dashboard-crNumber-card">
+                                {crNumberSummary?.totalScannedQty || 0}
+                            </p>
                         </div>
                     </div>
-                    <div style={{ marginTop: "10px" }}>
-                        <UiTable columns={DASHBOARD_TABLE_HEADERS()} dataSource={crNumberSummary?.fimNumberSummaryList} scroll={{ y: 300 }} />
+                    <div className="dashboard-table">
+                        <UiTable
+                            columns={DASHBOARD_TABLE_HEADERS()}
+                            dataSource={crNumberSummary?.fimNumberSummaryList}
+                            scroll={{ x: "max-content", y: 300 }}
+                        />
                     </div>
                 </div>
             </div>
