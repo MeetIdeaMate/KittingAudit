64a088afdba4f79ec4934540fa39b3ceb804eee4
chandrusanjiv <chandrusanjeev5@gmail.com>
2026-04-03 23:10:46 +0530

    kitting correction

--- src/pages/main/Kitting/avlPartModal/index.js
+++ src/pages/main/Kitting/avlPartModal/index.js
@@ -10,49 +10,49 @@ export const GetAvlParts = ({
 }) => {
     return <React.Fragment>
         <Skeleton active={isLoading} loading={isLoading}>
-        <div style={{display: "flex", justifyContent: "space-between", alignItems: "center"}}>
-            <div>
-                <span>Part Number</span>
-                <h3 style={{border: 0, margin: 0}}>{selectedPartDetails?.partNumber}</h3>
-                <UiTextBox placeholder="Bar Code ID" onKeyPress={(field) => hanldePressEnter(field)} onChange={(field) => handleChangeAvlBarcode(field)}/>
-            </div>
-            <div>
-                <span>Qty</span>
-                <h3 style={{border: 0, margin: 0}}>{selectedPartDetails?.quantity}</h3>
-            </div>
-        </div>
-        <div style={{display: "flex", justifyContent: "space-between"}}>
-            <h2 style={{padding: 0, margin: 0, width: "50%"}}>Available Parts</h2>
-            <h2 style={{padding: 0, margin: 0, width: "50%"}}>Missing Parts</h2>
-        </div>
-        <hr></hr>
-        <div style={{width: "100%", display: "flex"}}>
-            <div style={{width: "50%"}}>
-                <div style={{display: "flex", justifyContent: "space-between", padding: "0 5px"}}>
-                    <span>Barcode ID</span>
-                    <span>Qty: </span>
+            <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+                <div>
+                    <span>Part Number</span>
+                    <h3 style={{ border: 0, margin: 0 }}>{selectedPartDetails?.partNumber}</h3>
+                    <UiTextBox placeholder="Bar Code ID" onKeyPress={(field) => hanldePressEnter(field)} onChange={(field) => handleChangeAvlBarcode(field)} />
                 </div>
-                <hr></hr>
-                {
-                    selectedPartDetails?.caseInfo?.[0]?.barcodes?.map((part, indx, arr) => {
-                        return <p key={indx} style={{borderBottom: indx !== arr.length - 1 ? "1px solid #ccc" : "none"}}>{part}</p>
-                    })
-                }
-            </div>
-            <div style={{width: "1px", backgroundColor: "#000"}}></div>
-            <div style={{width: "49%"}}>
-                <div style={{display: "flex", justifyContent: "space-between", padding: "0 5px"}}>
-                    <span>Barcode ID</span>
-                    <span>Qty: </span>
+                <div>
+                    <span>Qty</span>
+                    <h3 style={{ border: 0, margin: 0 }}>{selectedPartDetails?.quantity}</h3>
                 </div>
-                <hr></hr>
-                {
-                    selectedPartDetails?.caseInfo?.[0]?.barcodes?.map((part, indx, arr) => {
-                        return <p key={indx} style={{paddingLeft: "5px ",borderBottom: indx !== arr.length - 1 ? "1px solid #ccc" : "none"}}>{part}</p>
-                    })
-                }
             </div>
-        </div>
+            <div style={{ display: "flex", justifyContent: "space-between" }}>
+                <h2 style={{ padding: 0, margin: 0, width: "50%" }}>Available Parts</h2>
+                <h2 style={{ padding: 0, margin: 0, width: "50%" }}>Missing Parts</h2>
+            </div>
+            <hr></hr>
+            <div style={{ width: "100%", display: "flex" }}>
+                <div style={{ width: "50%" }}>
+                    <div style={{ display: "flex", justifyContent: "space-between", padding: "0 5px" }}>
+                        <span>Barcode ID</span>
+                        <span>Qty: </span>
+                    </div>
+                    <hr></hr>
+                    {
+                        selectedPartDetails?.caseInfo?.[0]?.barcodes?.map((part, indx, arr) => {
+                            return <p key={indx} style={{ borderBottom: indx !== arr.length - 1 ? "1px solid #ccc" : "none" }}>{part?.part}</p>
+                        })
+                    }
+                </div>
+                <div style={{ width: "1px", backgroundColor: "#000" }}></div>
+                <div style={{ width: "49%" }}>
+                    <div style={{ display: "flex", justifyContent: "space-between", padding: "0 5px" }}>
+                        <span>Barcode ID</span>
+                        <span>Qty: </span>
+                    </div>
+                    <hr></hr>
+                    {
+                        selectedPartDetails?.caseInfo?.[0]?.barcodes?.map((part, indx, arr) => {
+                            return <p key={indx} style={{ paddingLeft: "5px ", borderBottom: indx !== arr.length - 1 ? "1px solid #ccc" : "none" }}>{part?.part}</p>
+                        })
+                    }
+                </div>
+            </div>
         </Skeleton>
     </React.Fragment>
 };
